4c770286e6d3d17d6aed82a91738d807e2404f24
jasnmar <jasnmar@gmail.com>
2024-04-06 14:30:39 -0400

    Default Param

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import { studentsArr } from '/studentsArr.js'
 import { itemsBoughtArr } from './itemsBoughtArr.js'
 
 
-function calculateTotalCost(itemsBoughtArr){
+function calculateTotalCost(itemsBoughtArr, discount=0){
 
         const total = itemsBoughtArr.reduce(function(total, item) {
             return total + item.priceUSD
         }, 0 )
-        return total
+        return total - discount
     }
     
 console.log(calculateTotalCost(itemsBoughtArr))
