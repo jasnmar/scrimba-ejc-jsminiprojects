47a2c3c2bbe33ee566e5ab8299369fbc1e0b6549
jasnmar <jasnmar@gmail.com>
2024-04-06 14:13:04 -0400

    reduce

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-import { interplanetaryDestinationsArr as destinations, shortSpaceTripsArr } from '/data.js'
+const grades = [75, 83, 66, 43, 55, 99, 87, 16, 89, 64, 70, 80, 94, 77, 66, 73]
 
-console.log(shortSpaceTripsArr)
+/*
+Challenge
+1. Use the .reduce() method to find the total of all of the students grades.
+2. Do some simple maths to log out the class average. 
+*/
+const total = grades.reduce(function(total, current) {
+    return total+current
+})
+
+const average = total / grades.length
+console.log(`The class average is ${average}`)
