03653e387eedf0df71caa0e5fc86be53f33fb681
jasnmar <jasnmar@gmail.com>
2024-04-06 13:57:00 -0400

    Nested arrow

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const speedWarning = (speedLimit, speed) => {
-    if(speed>speedLimit) {
-        return `You are going at ${speed} mph!`
-    }    
-}
+const distanceTraveledMiles = [267, 345, 234, 190, 299]
 
-console.log(speedWarning(30, 40))
+const distanceTraveledKm = distanceTraveledMiles.map((distance) => Math.round(distance * 1.6))
+
+console.log(distanceTraveledKm)
 
 /*
 Challenge
-1. Refactor this function to use an arrow function.
-2. Make sure you write the least code possible.
+1. Refactor this .map method so the inline function is
+   an arrow function. 
+2. Write the least amount of code possible.
 */
