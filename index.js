1d408cd4ba23045c177853dc53b63a5c493ded36
jasnmar <jasnmar@gmail.com>
2024-04-07 11:10:25 -0400

    Constructor Start

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const dateSnapshot = new Date().getFullYear()
-console.log(`Copyright ${dateSnapshot.toString()}`)
-
-
 /*
 Challenge:
-1. Search online to find out how we can get just the year 
-   using the Date() constructor and update the console.log above.
+1. Create a constructor function called 'Character'.
+2. Give it 'name' and 'collectedItemsArr' properties. 
+    - 'name' should hold the character’s name.
+	- 'collectedItemsArr' should hold an array of items. 
+       Initialise it to an empty array.
+3. Create an 'addItem' method which takes in an item as 
+   a parameter and adds it to 'collectedItemsArr'.
+4. The addItem method should log out a sentence like 
+   `Merlin now has: wand, map, potion`.
+5. Check it’s working by creating several instances of 
+   Character and adding items to their arrays.
 */
