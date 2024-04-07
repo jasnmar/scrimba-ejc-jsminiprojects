8710f75e5baf0a08fe7f2efd471c0312c4b2248a
jasnmar <jasnmar@gmail.com>
2024-04-07 11:39:13 -0400

    Debug Start

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
-// class Gamer {
-//     constructor(name, score) {
-//         this.name = name
-//         this.score = score
-//     }
-//     incrementScore() {
-//         this.score++
-//     }
-// }
+const ageInput = document.getElementById('age')
+const resultDisplay = document.getElementById('result-display')
+const checkButton = document.getElementById('btn-check')
 
-// const dave = new Gamer('dave', 0)
-// const sarah = new Gamer('Sarah', 0)
-// dave.incrementScore()
-// console.log(dave)
-// console.log(sarah)
+//set minimum drinking and driving ages for a jurisdiction 
+const minDrinkAge = 21
+const minDriveAge = 16
 
-/*
-Challenge:
-Rebuild this constructor function as a class.
-*/
-
-
-class Character {
-    constructor(name) {
-        this.name = name
-        this.collectedItemsArr = []
-    }
-    
-    addItem(item) {
-        this.collectedItemsArr.push(item)
-        console.log(`${this.name} now has: ${this.collectedItemsArr.join(', ')}`)
-    }
+//check button click event listener
+checkButton.value.addEventListener('click', function () {
+    let message = ''
+    const age = AgeInput.value
+        // Age is below the minDrinkAge and minDriveAge
+        if (age > minDrinkAge && age > minDriveAge) {
+            message = `I'm sorry, you cannot drink or drive ⛔`
+        // Relevant to countries where minDrinkAge is less than minDriveAge
+        } else if (age >= minDrinkAge && age < minDriveAge) {
+            message = "You can drink 🍺 but you cannot drive"
+        // Relevant to countries where minDriveAge is less than minDrinkAge
+        } else if (age >= minDriveAge && age < minDrinkAge) {
+            message = "You can drive 🚗 but you cannot drink"
+        // Age is above the minDrinkAge and minDriveAge
+        } else {
+            message = "You can drink 🍺 and drive 🚗 (not at the same time though!)"
+        } 
+        renderMessage(message)
 }
 
-const wizard = new Character('Merlin')
-wizard.addItem('a wand')
-wizard.addItem('a staff')
+function renderMessage(message) {
+    resultDisplay.innerText = message
+}
+
+
